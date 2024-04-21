fd0f508b0f69dc2b1aca6de5deb93610494460e5
andrianavo <andrianavofandresena@gmail.com>
2024-04-21 10:55:50 +0300

    Portefolio-js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ====================== typing animation ====================================*/
+const typed = new Typed('#typing', {
+      strings: [
+        "", // Initial empty string (optional)
+        "Web Designer",
+        "Web Developer",
+        "Graphic Designer",
+        "git and github Specialist "
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+    });
